21d8437e4c162c70ca34740302d39cb80b1a7b25
Dustin Weber <webedust@isu.edu>
2022-01-26 08:18:00 -0700

    Cleaned up main Program class

--- Tank/Program.cs
+++ Tank/Program.cs
@@ -8,9 +8,7 @@ namespace Tank
 {
     static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        /// <summary> The main entry point for the application. </summary>
         [STAThread]
         static void Main()
         {
