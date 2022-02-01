6e9bd54f545398b397fee2b527653578c3e194d2
evankaid <60327139+evankaid@users.noreply.github.com>
2022-02-01 15:57:35 -0700

    Created framework for the Tank Class
    Added variables and methods.

---
+++ Tank/Tank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank
+{
+    class Tank
+    {
+        public static float speed;
+        public static float playerHealth;
+        public static float enemyHealth;
+        public static float fireRate;
+        public static float projectileDamage;
+        public static bool isDead = false;
+        public static bool goLeft;
+        public static bool goRight;
+        public static bool goUp;
+        public static bool goDown;
+
+
+
+
+        public void takeDamage(float projectileDamage)
+        {
+            //Add logic to determine which entity got hit
+            playerHealth -= projectileDamage;
+
+            enemyHealth -= projectileDamage;
+        }
+
+        private void playerDeath()
+        {
+            if(playerHealth <= 0)
+            {
+                isDead = true;
+                //Display game over 
+            }
+        }
+
+        private void enemyDeath()
+        {
+            if(enemyHealth <= 0)
+            {
+                isDead = true;
+                //Remove dead enemy from the map
+            }
+        }
+
+        private void shoot()
+        {
+            //Add code for firing the projectile 
+        }
+    }
+}
