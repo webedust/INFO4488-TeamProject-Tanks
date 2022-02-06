82c18bfa6a2b6aad27c29df61e642f66a9863108
evankaid <60327139+evankaid@users.noreply.github.com>
2022-02-06 16:49:16 -0700

    Updated the names of form objects. Added player tank movement triggers (Not finished).

--- Tank/Form1.cs
+++ Tank/Form1.cs
@@ -17,5 +17,6 @@ namespace Tank
             InitializeComponent();
         }
 
+       
     }
 }
--- Tank/Tank.cs
+++ Tank/Tank.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Tank
 {
@@ -13,13 +14,69 @@ namespace Tank
         public float enemyHealth;
         public float fireRate;
         public float projectileDamage;
-        
-        public bool goLeft;
-        public bool goRight;
-        public bool goUp;
-        public bool goDown;
+
+        public string direction = "up";
+        //public bool goLeft;
+        //public bool goRight;
+        //public bool goUp;
+        //public bool goDown;
+        public bool gameOver;
 
 
+        //Need to add collision detection to movement and figure out how to attach
+        //KeyIsDown and KeyIsUp events to the playerTank picturebox on the form
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                //goLeft = true;
+                direction = "left";
+                playerTank.Image = Properties.Resources.PlayerTankLeft;
+
+                
+                if(playerTank.Left > 0)
+                {
+                    playerTank.Left -= speed;
+                }
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                //goRight = true;
+                direction = "right";
+                playerTank.Image = Properties.Resources.PlayerTankRight;
+
+                if (playerTank.Right > 0)
+                {
+                    playerTank.Right += speed;
+                }
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                //goUp = true;
+                direction = "up";
+                playerTank.Image = Properties.Resources.PlayerTankUp;
+
+                if (playerTank.Up > 0)
+                {
+                    playerTank.Up -= speed;
+                }
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                //goDown = true;
+                direction = "down";
+                playerTank.Image = Properties.Resources.PlayerTankDown;
+
+                if (playerTank.Down > 0)
+                {
+                    playerTank.Down += speed;
+                }
+            }
+        }
 
 
         public void takeDamage(float projectileDamage)
