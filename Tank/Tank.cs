d31c2cd5108311a85cf46d0aa37ec7c59c1e4575
Dustin Weber <webedust@isu.edu>
2022-02-07 08:14:56 -0700

    Changed Tank
    Tank now moves a PictureBox when the player movement keys are pressed. Start of the collision testing function (TryMove) was added.

--- Tank/Tank.cs
+++ Tank/Tank.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,8 +22,27 @@ namespace Tank
         //public bool goUp;
         //public bool goDown;
         public bool gameOver;
+        #region References
+        GameHandler gh;
+        PictureBox pictureBox;
+        public PictureBox PictureBox
+        {
+            /// <summary> PictureBox being used to render this Tank. </summary>
+            get { return pictureBox; }
+        }
+        #endregion
 
 
+        #region Initial
+        /// <summary> Creates a new tank. </summary>
+        /// <param name="gh"> Reference to the GameHandler running the current form. </param>
+        /// <param name="pic"> PictureBox to use for rendering this tank to the form. </param>
+        public Tank(GameHandler gh, PictureBox pic)
+        {
+            this.gh = gh;
+            pictureBox = pic;
+        }
+        #endregion
         //Need to add collision detection to movement and figure out how to attach
         //KeyIsDown and KeyIsUp events to the playerTank picturebox on the form
 
@@ -32,24 +52,22 @@ namespace Tank
             {
                 //goLeft = true;
                 direction = "left";
-                playerTank.Image = Properties.Resources.PlayerTankLeft;
+                pictureBox.Image = Properties.Resources.PlayerTankLeft;
 
-                
-                if(playerTank.Left > 0)
-                {
-                    playerTank.Left -= speed;
-                }
+                if (pictureBox.Left > 0)
+                    pictureBox.Left -= (int)speed;
             }
 
             if (e.KeyCode == Keys.Right)
             {
                 //goRight = true;
                 direction = "right";
-                playerTank.Image = Properties.Resources.PlayerTankRight;
+                pictureBox.Image = Properties.Resources.PlayerTankRight;
 
-                if (playerTank.Right > 0)
+                if (pictureBox.Right > 0)
                 {
-                    playerTank.Right += speed;
+                    // Modifying from left because it's the only public property
+                    pictureBox.Left += (int)speed;
                 }
             }
 
@@ -57,11 +75,11 @@ namespace Tank
             {
                 //goUp = true;
                 direction = "up";
-                playerTank.Image = Properties.Resources.PlayerTankUp;
+                pictureBox.Image = Properties.Resources.PlayerTankUp;
 
-                if (playerTank.Up > 0)
+                if (pictureBox.Top > 0)
                 {
-                    playerTank.Up -= speed;
+                    pictureBox.Top -= (int)speed;
                 }
             }
 
@@ -69,11 +87,12 @@ namespace Tank
             {
                 //goDown = true;
                 direction = "down";
-                playerTank.Image = Properties.Resources.PlayerTankDown;
+                pictureBox.Image = Properties.Resources.PlayerTankDown;
 
-                if (playerTank.Down > 0)
+                if (pictureBox.Bottom > 0)
                 {
-                    playerTank.Down += speed;
+                    // Modifying from top because it's the only public property
+                    pictureBox.Top += (int)speed;
                 }
             }
         }
@@ -108,6 +127,28 @@ namespace Tank
         private void shoot()
         {
             //Add code for firing the projectile 
+        }
+        /// <summary> Attempts to move towards the specified position. </summary>
+        /// <param name="a"> Current position. </param>
+        /// <param name="b"> Position to move towards. </param>
+        /// <returns> True if successfully moved. False if there's an obstacle in the way. </returns>
+        public bool TryMove(Point a, Point b)
+        {
+            // Convert point. May not be needed so commented out
+            // uncomment if movement and collision testing seems odd.
+            //a = PictureBox.PointToClient(a);
+
+            foreach (Collider col in gh.Colliders)
+            {
+                // Ignore any distant colliders to save memory usage
+                if (// To-do: Write a utils function for distance here)
+
+                // To-do: Collision testing will go here
+            }
+
+            // Set new position if no colliders are in the way
+            PictureBox.Location = b;
+            return true;
         }
     }
 }
